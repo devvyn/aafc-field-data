ea77cb03ba098e9411cb3f518febc7c496cbc62d
Devvyn Patrick Murphy <devvyn@devvyn.io>
2019-08-01 17:39:53 -0600

    refactor, reformat, rearrange, add doc strings
    - simplify types
    - use absolute imports

--- projects/2016-sweep-vs-tiller/helper.py
+++ projects/2016-sweep-vs-tiller/helper.py
@@ -1,103 +1,70 @@
+"""Functions used by scripts analysing Excel spreadsheet data."""
 import re
-from typing import Dict, Mapping, Iterable, Any
+from typing import Iterable
 
-from pandas import ExcelFile, DataFrame
-from pandas import np
+import pandas
 
-NaN = np.nan
-
-mapping_str_any_ = Mapping[str, Any]
-
-sheet_dict = Mapping[str, mapping_str_any_]
+nan = pandas.np.nan
 
 
-def get_sheets(filename: str, names: Iterable[str] = None) -> (
-        sheet_dict):
+def get_sheets(filename: str, names: Iterable):
     """
     Return a mapping of `names` and corresponding DataFrames made from sheets
     in the spreadsheet specified by `filename`.
     """
-    data_file = ExcelFile(filename)
-    sheet_names = names or data_file.sheet_names
-    return {
-        sheet_name: data_file.parse(sheet_name)
-        for sheet_name in sheet_names
-    }
+    data_file = pandas.ExcelFile(filename)
+    return {name: data_file.parse(name)
+            for name in names}
 
 
-def emojify_boolean(frame: DataFrame) -> DataFrame:
-    return (frame.replace({
-        True: '✅', False: '✖️',
-    }))
+def normalize_str(value: str, separator: str = ' ') -> str:
+    """Transform string from camel-case to lowercase alphanumeric."""
+    return convert_from_plural(
+        re.compile(r'[^a-zA-Z0-9]').sub(
+            separator, convert_from_camel_case(
+                separator, str(value), ).lower()))
 
 
-def compare_columns(frames: Dict[str, DataFrame]) -> DataFrame:
-    return emojify_boolean(DataFrame(data={name: sheet.columns.to_series(
-        name=name) for name, sheet in frames.items()}).notna())
-
-
-def len_unique(pandas_object):
-    return len(pandas_object.unique())
-
-
-def show_spaces(x):
-    return x.str.replace(' ', '⬜️')
-
-
-def normalize_str(value, separator: str = ' ') -> str:
-    if value == NaN:
-        return value
-    return convert_from_plural(replace_non_alphanumeric(separator,
-        convert_from_camel_case(separator, str(value))))
-
-
-def convert_from_plural(transformed: str) -> str:
+def convert_from_plural(string: str) -> str:
+    """
+    Singularize nouns in label string
+    """
     # @todo: replace with regex
-    if transformed.endswith('oats'):
-        transformed = transformed[:-1]
-    return transformed
-
-
-def replace_non_alphanumeric(separator: str, str_value: str) -> str:
-    transformed: str = re.compile(r'[^a-zA-Z0-9]').sub(separator,
-        str_value.lower())
-    return transformed
+    if string.endswith('oats'):
+        string: str = string[:-1]
+    return string
 
 
 def convert_from_camel_case(separator: str, string: str) -> str:
     """
     Convert all words from camel-case to lowercase.
     """
-    is_mixed_case: bool = (
-        not (str_value.islower() or str_value.isupper()) and (
-            str_value.upper() != str_value.lower()))
-    if is_mixed_case:
-        # noinspection PyTypeChecker
-        word_index = [
-                         # Remember starting positions of all uppercase
-                         # characters.
-                         index for index, char in enumerate(string)
-                         if char.isupper()
-                     ] + [
-                         None,
-                     ]  # Final null for easier string slicing with [
-        # …:word_index[i + 1]].
+    if not (string.islower() or
+            string.isupper() or
+            string.lower() == string.upper()):
+        indices = [index for index, char in enumerate(string)
+                   if char.isupper()]
+        indices.append(len(string))
+        word_index = indices
         if word_index:
-            words = [  # Strip leading and trailing whitespace from each word.
-                string[word_index[i]:word_index[i + 1]].strip() for i in
-                range(len(word_index) - 1)]
-            string = separator.join(words)  # Rejoin words.
+            string = separator.join(
+                [string[word_index[i]:word_index[i + 1]].strip()
+                 for i in range(len(word_index) - 1)])
     return string
 
 
-def alphanumeric_lower(value):
-    return re.compile(r"[^a-z0-9]").sub('', str(value).lower())
+def alphanumeric_lower(value: str) -> str:
+    """Chained functions `str.lower()` and `alphanumeric()`."""
+    return alphanumeric(str(value).lower())
 
 
-def drop_except(frame: DataFrame, columns: list) -> DataFrame:
+def alphanumeric(string: str) -> str:
+    """Remove all non-alphanumeric characters."""
+    return re.compile(r"[^a-zA-Z0-9]").sub('', string)
+
+
+def drop_except(frame: pandas.DataFrame, columns: list) -> pandas.DataFrame:
     """
     Drop all columns from `frame` not named in `columns`.
     """
-    return frame.drop(columns=(
-        frame.columns[~ frame.columns.isin(columns)]
-    ))
+    return frame.drop(columns=(frame.columns[~ frame.columns.isin(columns)]))
