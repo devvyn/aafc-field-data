87833509a5325d763afc47c6cd335ee759bd9e9d
Devvyn Patrick Murphy <devvyn@devvyn.io>
2019-07-31 22:05:40 -0600

    reformat

--- projects/2016-sweep-vs-tiller/helper.py
+++ projects/2016-sweep-vs-tiller/helper.py
@@ -1,18 +1,28 @@
 import re
-from typing import Dict, Mapping, Iterable
+from typing import Dict, Mapping, Iterable, Any
 
 from pandas import ExcelFile, DataFrame
 from pandas import np
 
 NaN = np.nan
 
+mapping_str_any_ = Mapping[str, Any]
+
+sheet_dict = Mapping[str, mapping_str_any_]
+
 
 def get_sheets(filename: str, names: Iterable[str] = None) -> (
-        Mapping[str, DataFrame]):
+        sheet_dict):
+    """
+    Return a mapping of `names` and corresponding DataFrames made from sheets
+    in the spreadsheet specified by `filename`.
+    """
     data_file = ExcelFile(filename)
     sheet_names = names or data_file.sheet_names
-    return {sheet_name: data_file.parse(sheet_name) for sheet_name in
-        sheet_names}
+    return {
+        sheet_name: data_file.parse(sheet_name)
+        for sheet_name in sheet_names
+    }
 
 
 def emojify_boolean(frame: DataFrame) -> DataFrame:
@@ -54,26 +64,40 @@ def replace_non_alphanumeric(separator: str, str_value: str) -> str:
     return transformed
 
 
-def convert_from_camel_case(separator: str, str_value: str) -> str:
+def convert_from_camel_case(separator: str, string: str) -> str:
+    """
+    Convert all words from camel-case to lowercase.
+    """
     is_mixed_case: bool = (
         not (str_value.islower() or str_value.isupper()) and (
             str_value.upper() != str_value.lower()))
     if is_mixed_case:
         # noinspection PyTypeChecker
         word_index = [
-            # Remember starting positions of all uppercase characters.
-            index for index, char in enumerate(str_value)
-            if char.isupper()
-        ] + [
-            None,
-        ]  # Final null for easier string slicing with […:word_index[i + 1]].
+                         # Remember starting positions of all uppercase
+                         # characters.
+                         index for index, char in enumerate(string)
+                         if char.isupper()
+                     ] + [
+                         None,
+                     ]  # Final null for easier string slicing with [
+        # …:word_index[i + 1]].
         if word_index:
             words = [  # Strip leading and trailing whitespace from each word.
-                str_value[word_index[i]:word_index[i + 1]].strip() for i in
+                string[word_index[i]:word_index[i + 1]].strip() for i in
                 range(len(word_index) - 1)]
-            str_value = separator.join(words)  # Rejoin words.
-    return str_value
+            string = separator.join(words)  # Rejoin words.
+    return string
 
 
 def alphanumeric_lower(value):
     return re.compile(r"[^a-z0-9]").sub('', str(value).lower())
+
+
+def drop_except(frame: DataFrame, columns: list) -> DataFrame:
+    """
+    Drop all columns from `frame` not named in `columns`.
+    """
+    return frame.drop(columns=(
+        frame.columns[~ frame.columns.isin(columns)]
+    ))
